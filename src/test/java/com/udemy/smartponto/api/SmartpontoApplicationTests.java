d09f03a953faafc3d9ba8001a0fa3b76e57ee805
Jefferson Augusto <jeffersonxplode@gmail.com>
2020-08-08 20:11:51 -0300

    Primeira ocnfiguração.

---
+++ src/test/java/com/udemy/smartponto/api/SmartpontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.smartponto.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartpontoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
